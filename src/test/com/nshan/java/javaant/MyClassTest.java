f2d337358488ab3282babeeebe747789851cdea5
nshan <nsstudev@gmail.com>
2014-04-06 22:17:15 +0200

    re-organized and added test directory

---
+++ src/test/com/nshan/java/javaant/MyClassTest.java
@@ -0,0 +1,10 @@
+package com.nshan.java.javaant;
+
+import junit.framework.TestCase;
+
+ 
+public class MyClassTest extends TestCase{
+	public void testMainMethod(){
+		assertEquals(true, true);
+	}
+}
