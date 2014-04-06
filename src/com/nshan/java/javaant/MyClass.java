623ac0f86049269e63c08076ac5fb7d8e216bac8
nshan <nsstudev@gmail.com>
2014-04-06 21:25:25 +0200

    removed unwanted files

--- src/com/nshan/java/javaant/MyClass.java
+++ src/com/nshan/java/javaant/MyClass.java
@@ -1,10 +1,17 @@
 package com.nshan.java.javaant;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public class MyClass {
 
+	static Logger logger = Logger.getLogger(MyClass.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BasicConfigurator.configure();
 		System.out.println("Welcome!!");
+		logger.info("Successfully configured Log4j in project");
 	}
 
 }
